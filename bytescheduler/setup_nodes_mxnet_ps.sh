2c0a2e2642beeae9a4ee2f952a65920d1a0afe90
ZYCCC927 <61256396+ZYCCC927@users.noreply.github.com>
2021-03-28 23:20:00 +0800

    Update setup_nodes_mxnet_ps.sh

--- bytescheduler/setup_nodes_mxnet_ps.sh
+++ bytescheduler/setup_nodes_mxnet_ps.sh
@@ -14,7 +14,7 @@ chmod 600 ~/.ssh/config
 chmod 600 ~/byteps/bytescheduler/bytescheduler-0105.pem
 id2translate=$(cat ~/.ssh/id_rsa.pub)
 ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.25.212 "echo $id2translate >>~/.ssh/authorized_keys"
-ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.20.518 "echo $id2translate >>~/.ssh/authorized_keys"
+ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.20.158 "echo $id2translate >>~/.ssh/authorized_keys"
 
 cd ~/
 # setup docker and connect to container
