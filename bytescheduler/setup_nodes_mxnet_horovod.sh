9addbb07197f4e4d82464492b5af479e5b7941c5
ZYCCC927 <61256396+ZYCCC927@users.noreply.github.com>
2021-05-20 13:11:39 +0800

    Update setup_nodes_mxnet_horovod.sh

--- bytescheduler/setup_nodes_mxnet_horovod.sh
+++ bytescheduler/setup_nodes_mxnet_horovod.sh
@@ -12,8 +12,8 @@ chmod 600 ~/.ssh/id_rsa
 chmod 600 ~/.ssh/config
 chmod 600 ~/byteps/bytescheduler/bytescheduler-0105.pem
 id2translate=$(cat ~/.ssh/id_rsa.pub)
-ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.7.131 "echo $id2translate >>~/.ssh/authorized_keys"
-ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.10.158 "echo $id2translate >>~/.ssh/authorized_keys"
+ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.0.225 "echo $id2translate >>~/.ssh/authorized_keys"
+ssh -i ~/byteps/bytescheduler/bytescheduler-0105.pem ubuntu@172.31.6.169 "echo $id2translate >>~/.ssh/authorized_keys"
 
 cd ~/
 # setup docker and connect to container
